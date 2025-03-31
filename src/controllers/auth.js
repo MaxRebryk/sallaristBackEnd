786dfa6de5bdef0f1473c94b3c15e66715dede9a
Max Rebryk <rebryk2003@gmail.com>
2025-03-31 09:24:35 +0300

    change login cookie

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -24,10 +24,15 @@ export const loginUserController = async (req, res) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
+    sameSite: 'None',
+    secure: true,
   });
+
   res.cookie('sessionId', session._id, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
+    sameSite: 'None',
+    secure: true,
   });
 
   res.json({
