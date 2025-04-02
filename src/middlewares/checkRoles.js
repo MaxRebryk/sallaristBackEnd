7587e5df4af85b84361d164014f5c24fca84da5d
Max Rebryk <rebryk2003@gmail.com>
2025-04-02 14:02:30 +0300

    change checkRoles logic

--- src/middlewares/checkRoles.js
+++ src/middlewares/checkRoles.js
@@ -8,31 +8,35 @@ export const checkRoles =
   async (req, res, next) => {
     const { user } = req;
     if (!user) {
-      next(createHttpError(401));
-      return;
+      return next(createHttpError(401));
     }
 
     const { role } = user;
+
     if (roles.includes(ROLES.OWNER) && role === ROLES.OWNER) {
-      next();
-      return;
+      return next();
     }
 
     if (roles.includes(ROLES.WORKER) && role === ROLES.WORKER) {
       const { userId } = req.params;
-      if (!userId) {
-        next(createHttpError(403));
-        return;
+
+      if (userId) {
+        const worker = await UsersCollection.findOne({
+          _id: userId,
+          parentId: user._id,
+        });
+        if (worker) {
+          return next();
+        }
       }
-      const worker = await UsersCollection.findOne({
-        _id: userId,
-        parentId: user._id,
-      });
-      if (worker) {
-        next();
-        return;
+
+      if (req.path === '/users') {
+        req.query.parentId = user._id.toString();
+        return next();
       }
+
+      return next(createHttpError(403));
     }
 
-    next(createHttpError(403));
+    return next(createHttpError(403));
   };
